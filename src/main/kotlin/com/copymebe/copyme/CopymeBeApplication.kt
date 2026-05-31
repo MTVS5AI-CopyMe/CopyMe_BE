5c1b51f028d0b0af859b138cbe3ce0039efd8c6d
la094 <devowl094@gmail.com>
2026-05-31 20:09:48 +0900

    init

---
+++ src/main/kotlin/com/copymebe/copyme/CopymeBeApplication.kt
@@ -0,0 +1,11 @@
+package com.copymebe.copyme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CopymeBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CopymeBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/copymebe/copyme/CopymeBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.copymebe.copyme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CopymeBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
