fe7d92e1526641d4545f9d76af742f05b0044c9e
Owl <devowl094@gmail.com>
2026-06-01 16:01:31 +0900

    rename

--- src/main/kotlin/com/copymebe/copyme/resources/member/signup/MemberSignupAuthCodeRequestController.kt
+++ src/main/kotlin/com/copymebe/copyme/resources/member/signup/MemberSignupAuthCodeRequestController.kt
@@ -1,38 +1,50 @@
 package com.copymebe.copyme.resources.member.signup
 
-import com.copymebe.copyme.core.domain.auth.SignupAuthCodeExpiredException
+import com.copymebe.copyme.core.domain.auth.MaxSignupAuthRequestExceededException
 import com.copymebe.copyme.core.domain.auth.SignupAuthenticationManagerRepo
 import com.copymebe.copyme.core.domain.auth.models.SignupAuthenticationManager
+import com.copymebe.copyme.core.domain.member.AlreadyExistsMemberException
+import com.copymebe.copyme.core.domain.member.MemberRepo
 import com.copymebe.copyme.core.global.http.CustomResponseEntity
 import com.copymebe.copyme.core.global.http.swagger.ApiExceptions
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import jakarta.validation.constraints.Email
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
-data class SignupAuthCodeValidateRequest(
+data class MemberSignupAuthCodeRequest(
+    @field:Email
     val email: String,
-    val authCode: String,
 )
 
 @Tag(name = "Member")
 @RestController
-class SignupAuthCodeValidateController(
+class MemberSignupAuthCodeRequestController(
+    private val memberRepo: MemberRepo,
     private val signupAuthenticationManagerRepo: SignupAuthenticationManagerRepo,
 ) {
-    @Operation(summary = "멤버 회원가입 인증코드 검증")
+    @Operation(summary = "멤버 회원가입 인증코드 요청")
     @ApiResponse(responseCode = "200")
     @ApiExceptions(
-        SignupAuthCodeExpiredException::class,
+        AlreadyExistsMemberException::class,
+        MaxSignupAuthRequestExceededException::class,
     )
-    @PostMapping("/members/signup/authcode-validate")
-    fun validateSignupAuthCode(
-        @RequestBody @Valid req: SignupAuthCodeValidateRequest
-    ): CustomResponseEntity<String> {
+    @PostMapping("/members/signup/authcode")
+    fun requestSignupAuthCode(
+        @RequestBody @Valid req: MemberSignupAuthCodeRequest
+    ): CustomResponseEntity<Boolean> {
         val email = req.email
+
+        // 이미 가입된 회원인지 확인
+        memberRepo
+            .findByEmail(email)
+            ?.let {
+                throw AlreadyExistsMemberException()
+            }
 
         // 회원가입 인증 매니저 불러오기
         val signupAuthenticationManager =
@@ -40,16 +52,14 @@ class SignupAuthCodeValidateController(
                 .findByEmail(email)
                 ?: SignupAuthenticationManager.create(email)
 
-        // 인증 매니저에 인증 요청
-        signupAuthenticationManager.authenticateOrThrow(req.authCode)
+        // 인증 매니저에 인증요청 추가
+        val newAuthCodeRequest = signupAuthenticationManager.addRequestOrThrow()
+        println(newAuthCodeRequest.authCode)
+        // TODO: 이메일로 AuthCode 전송하기
 
         // 저장
         signupAuthenticationManagerRepo.save(signupAuthenticationManager)
 
-        // 이메일 인증토큰
-        // TODO: JWT로 변경하기
-        val emailAuthToken = "asdasd123"
-
-        return CustomResponseEntity(data = emailAuthToken)
+        return CustomResponseEntity(data = true)
     }
 }
