9c97a840d8ba0d10d8a06299b02af1a20f34b489
la094 <devowl094@gmail.com>
2026-06-03 22:01:27 +0900

    feat(API): 퀘스트 응답 검색 API

---
+++ src/main/kotlin/com/copymebe/copyme/api/quest/quest_answer/QuestAnswerSearchVSA.kt
@@ -0,0 +1,57 @@
+package com.copymebe.copyme.api.quest.quest_answer
+
+import com.copymebe.copyme.core.domain.quest.quest_answer.QuestAnswerRepo
+import com.copymebe.copyme.core.domain.quest.quest_answer.models.QuestAnswer
+import com.copymebe.copyme.core.global.http.CustomResponseEntity
+import com.copymebe.copyme.core.global.http.swagger.SwaggerSecurityConst
+import com.copymebe.copyme.core.global.pagination.OffsetPage
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.data.domain.PageRequest
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+data class QuestAnswerSearchRequest(
+    @Schema(description = "페이지 인덱스", defaultValue = "0")
+    val page: Int,
+
+    @Schema(description = "페이지 사이즈", defaultValue = "10")
+    val size: Int,
+
+    @Schema(
+        description = "유저 ID",
+        required = false,
+        example = "123e4567-e89b-12d3-a456-426614174000"
+    )
+    val memberId: UUID?
+)
+
+@SecurityRequirement(name = SwaggerSecurityConst.BEARER_AUTH)
+@Tag(name = "Quest")
+@RestController
+class QuestAnswerSearchVSA(
+    private val questAnswerRepo: QuestAnswerRepo
+) {
+    @Operation(summary = "퀘스트 응답 검색")
+    @GetMapping("/quest-answers")
+    fun search(@Valid req: QuestAnswerSearchRequest): CustomResponseEntity<OffsetPage<List<QuestAnswer>>> {
+        val pageable = PageRequest.of(req.page, req.size)
+
+        val r = when {
+            req.memberId != null ->
+                questAnswerRepo.findAllByMemberId(
+                    memberId = req.memberId,
+                    pageable = pageable
+                )
+
+            else ->
+                questAnswerRepo.findAll(pageable)
+        }.let { OffsetPage.create(it) }
+
+        return CustomResponseEntity(data = r)
+    }
+}
--- src/main/kotlin/com/copymebe/copyme/core/domain/quest/quest_answer/QuestAnswerRepo.kt
+++ src/main/kotlin/com/copymebe/copyme/core/domain/quest/quest_answer/QuestAnswerRepo.kt
@@ -1,7 +1,11 @@
 package com.copymebe.copyme.core.domain.quest.quest_answer
 
 import com.copymebe.copyme.core.domain.quest.quest_answer.models.QuestAnswer
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface QuestAnswerRepo : JpaRepository<QuestAnswer, UUID>
+interface QuestAnswerRepo : JpaRepository<QuestAnswer, UUID> {
+    fun findAllByMemberId(memberId: UUID?, pageable: Pageable): Page<QuestAnswer>
+}
